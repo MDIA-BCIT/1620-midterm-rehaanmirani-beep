0a105e1114ea30a5f694a7d37507a412f0dad143
rehaan mirani <you@example.com>
2025-10-29 09:19:41 -0700

    FInished midterm code in probelm.js

--- problem.js
+++ problem.js
@@ -1,15 +1,23 @@
-/*
-Create a function that simulates a simple password checking system. The function will take in a password and a user's input as parameters. It will then compare the user's input to the password.
 
-- If the user's input matches the password exactly, log out "Access Granted!"
-- If the user's input does not match, log out "Access Denied!"
-- If the user's input is "forgot", log out "Here is a hint"
-- If the user's input is "reset", log out "Let's reset your account"
+function passwordCheck(password, userInput) {
 
-Invoke examples of the function that fullfil each condition listed above. Feel free to work locally using node, and then paste your code into this file when it runs.
+    if (userInput === password) {
+        console.log("Access Granted!");
 
-CHALLENGE
-- If the password set is "forgot" or "reset", aside from "Access Granted", also log: "This password you set should not be used because it glitches the system".
-- If the password is less than 5 characters, log out "Your password is too short!".
-*/
+    } else if (userInput === "forgot") {
+        console.log("Here is a hint");
+    } else if (userInput === "reset") {
+        console.log("Let's reset your account");
+    } else {
+        console.log("Access Denied!");
+    }
 
+    if (password.length < 5) {
+        console.log("Your password is too short!");
+        return;
+    }
+
+    if (password === "forgot" || password === "reset") {
+        console.log("This password you set should not be used because it glitches the system");
+    }
+}
